573cdd5545781cdc981fe8bba06009f5f39abe60
Riaan33 <riaanlonghani3@gmail.com>
2025-08-16 14:15:44 -0400

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,36 @@
+##For Loop – Print Numbers 1 to 5
+
+forloop:()
+print("1")
+print("2")
+print("3")
+print("4")
+print("5")
+##While Loop – Countdown
+
+whileloop:()
+number=(input("Enter number here"))
+print(number)
+print(1)
+##List – Sum of Items
+
+list1=[1,2,3]
+print(6)
+##Try and Except – Safe Division
+
+a=(input("Enetr numer here"))
+b=(input("Enter number here"))
+
+answer=a/b
+if a or b==0:
+    print("Cannot divied by zero!")
+else:
+    print(answer)
+##Sum of Two Numbers
+answer1=a+b
+print(answer1)
+##Even or Odd
+
+
+
+    
